fed74d24940443642a9f19f0e46685b8c8038915
oguenguer <oguenguer@student.tgm.ac.at>
2018-01-30 10:47:06 +0100

    B03_HelloWorldTeam.java

---
+++ B03_HelloWorldTeam.java
@@ -0,0 +1,10 @@
+package GIT;
+
+Publi class Hallo {
+	
+	public static void Hallo(){
+		System.out.println("Güngür lässt schön Grüßen");
+	}
+	public static void main (String[]args){
+		Hallo();
+	}
